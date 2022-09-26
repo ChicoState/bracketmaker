9f7153019b8b8d98c025a2b4cd8af4e5f6c04d90
Paul Robert Davis <pauldavis1826@gmail.com>
2022-09-26 13:00:32 -0700

    upgrated react and webpack - hotloading is working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from "react"
 
 import Ribbon from "./Ribbon"
-import TeamCards from "./TeamCards"
-import PlayerCards from "./PlayerCards"
-import PlayerStats from "./PlayerStats"
-
-//TODO
-// 1) back button at each state to previous state or  Nav bar with Hyperlinks
-// 2) sort the player list somehow - maybe a grid where you can sort by name, or position
-// 3) build out the player card like its a baseball card, try to make it pretty
-
-
-// Idea - it would be nice to approach this as a in game aid for the viewer
-// could be based on the active lineup on both sides? 
-
-
 
 class App extends React.Component{
     constructor(props){
@@ -33,11 +19,6 @@ class App extends React.Component{
         this.handlePlayerClick = this.handlePlayerClick.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         
-        //states to move through the levels of drill down
-            //searchYear
-            //selectTeam
-            //selectPlayer
-            //viewPlayerStats
     }
 
     handleTeamClick(teamID){
@@ -51,136 +32,6 @@ class App extends React.Component{
     handleSubmit(event){
         this.getTeams();
         event.preventDefault();
-    }
-
-    //TODO change these urls to const variables for clearing code
-    getTeams(){
-        let rosterYear = document.getElementById('RosterYear').value;                
-        fetch(`https://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code=%27mlb%27&all_star_sw=%27N%27&sort_order=name_asc&season=%27${rosterYear}%27`)
-            .then(prom =>prom.json())
-            .then(data => {  
-                console.log('set state select team')
-                console.log(data.team_all_season.queryResults.row)
-                this.setState({
-                    drillDown: 'selectTeam',
-                    data: data.team_all_season.queryResults.row
-                });
-            })
-            .catch(err => console.log('Error: '+err));
-    }
-
-    getPlayers(teamID){
-        let rosterYear = document.getElementById('RosterYear').value;
-        fetch(`https://lookup-service-prod.mlb.com/json/named.roster_team_alltime.bam?start_season=%27${rosterYear-1}%27&end_season=%27${rosterYear}%27&team_id=%27${teamID}%27`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.roster_team_alltime.queryResults.row)
-                this.setState({
-                    drillDown: 'selectPlayer',
-                    data: data.roster_team_alltime.queryResults.row
-                })
-            })
-            .catch(err => console.log('Error: '+err));
-    }
-
-    getPlayer(playerID){
-        let rosterYear = document.getElementById('RosterYear').value;
-        //Sample PlayerID --> Madison Bumgarner: 518516
-
-        //Define all of the URLS
-            //player info          http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code=%27mlb%27&player_id=%27493316%27
-            const playerInfoAPI = `https://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code=%27mlb%27&player_id=%27${playerID}%27`
-                /*
-                    name_nick
-                    primary_stat_type
-                    status
-                */
-            
-            //season hitting http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%272017%27&player_id=%27493316%27
-            const seasonHittingAPI = `https://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%27${rosterYear}%27&player_id=%27${playerID}%27`
-            
-            //season pitching 
-            const seasonPitchingAPI = `https://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%27${rosterYear}%27&player_id=%27${playerID}%27`
-            //Sample PlayerID --> Madison Bumgarner: 518516
-            /*
-                
-               g  - Games
-               gs - Games Started
-               qs -  Quality Starts
-               bqs - Blown Quality Starts
-               ip - Innings Pitched
-                
-                Total Batters
-               so - Strike Outs
-               bb - Base on Balls 
-               np - Total Pitches
-               hb - hit batters
-
-               h   - Hits
-               db  - Doubles
-                Tripples
-               hr   - Home Runs
-               gs   - Grand Slams
-               r    - Runs
-               er   - Earned Runs
-               gidp - GIDP (ground into douple play)
-
-
-                bb9 - BB/9
-                k9 - k/9 strikes ber inning 
-                kbb - strike to walk ratio
-                rs9 - rs/9
-                h9  - H/9
-                hr9 - HR/9
-            */
-
-            //career hitting 'http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%'+playerID+'%27'
-            const careerHittingAPI = `https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerID}%27`
-            
-            //career pitching  http://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='592789'
-            const careerPitchingAPI =`https://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerID}%27`
-            
-            //projected hitting https://appac.github.io/mlb-data-api-docs/#stats-data-projected-hitting-stats-get
-            const projectedHittingAPI =`https://lookup-service-prod.mlb.com/json/named.proj_pecota_batting.bam?season=%27${rosterYear}%27&player_id=%27${playerID}%27`
-            
-            //projected pitching https://appac.github.io/mlb-data-api-docs/#stats-data-projected-pitching-stats-get
-            const projectedPitchingAPI = `https://lookup-service-prod.mlb.com/json/named.proj_pecota_pitching.bam?season=%27${rosterYear}%27&player_id=%27${playerID}%27`      
-
-            //initialize and array as a collection of all of the urls
-            let urls = [
-                playerInfoAPI,
-                seasonHittingAPI,
-                seasonPitchingAPI,
-                careerHittingAPI,
-                careerPitchingAPI,
-                projectedHittingAPI,
-                projectedPitchingAPI
-            ]
-
-            //iterate over the array to fetch all of the URLs
-            let requests = urls.map(url => fetch(url));
-
-            //use promises.all to wait for a response from all of the fetch promises
-           Promise.all(requests)
-            .then(function (responses) {
-                return Promise.all(responses.map(function (response) {
-                    return response.json()
-                }))
-            })
-            .then(data => {
-               console.log(data)
-                this.setState({
-                    drillDown: 'playerStats',
-                    data:               data[0].player_info.queryResults.row,
-                    seasonHittingData:  data[1].sport_hitting_tm.queryResults.row,
-                    seasonPitchingData: data[2].sport_pitching_tm.queryResults.row,
-                    careerHittingData:  data[3].sport_career_hitting.queryResults.row,
-                    careerPitchingData: data[4].sport_career_pitching.queryResults.row,
-                    projHittingData:    data[5].proj_pecota_batting.queryResults.row,
-                    projPitchingData:   data[6].proj_pecota_pitching.queryResults.row
-                })
-            })
-            .catch(error => console.log(`error: ${error}`))        
     }
     
     render(){
@@ -198,12 +49,12 @@ class App extends React.Component{
 
            // parentDivClassName='form-group'
             labelFor='MLB Roster Year'            
-            label= 'MLB Year'
+            label= 'Brackemaker'
             searchBartype='number'
             searchBarClassName='form-control'
             searchBarID='RosterYear'
             //placeholder=
-            defaultValue='2021'
+            defaultValue='...'
             
 
             //Submit Button props
@@ -287,13 +138,7 @@ S                            jerseyNumber={jersey_number}
                         in={inches}
                         weight={data['weight']}
                         jerseyNumber={data['jersey_number']}
-  /* 7/13/20 ready to add the props in to display the data, just look it over to get the syntax right for eeach datapoint.                        
-  //                      games={seasonPitchingData['g']}
-  //                      gamesStarted={seasonPitchingData['gs']}
-  //                      qualityStarts={seasonPitchingData['qs']}
-  //                      blownQualityStarts={seasonPitchingData['bqs']}
-  //                      inningsPitched={seasonPitchingData['ip']}
-*/                              
+                          
                     />
                         
                 break;
