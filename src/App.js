ddf61bbd93c38bc3c21b63db1f5c34f6efce8995
Cameron Wright <mr.wrightcgw@gmail.com>
2022-10-01 03:08:05 -0700

    mistakenly missed files that are required

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import React from "react"
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import View from "./view";
+import React from "react";
+import Player from "./add";
+import PlayerList from "./players";
+import Match from "./match";
+import { firestore } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-import Ribbon from "./Ribbon"
-import PlayerList from './players'
-import Match from './match'
-
-function randomizer(players) {
-  console.log(players)
-  var range = players.length;
-  while (range != 0) {
-    var random = Math.floor(Math.random() * range);
-    range--;
-    [players[range], players[random]] = [players[random], players[range]];
-  }
-  return players;
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Navbar } from "react-bootstrap";
 
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: ['Alice', 'Billy', 'Tommy', 'Gwen', 'Amanda', 'Chris', 'Kyle'],
+function App() {
+  const [players, updatePlayers] = useState([]);
+  const [matches, updateMatches] = useState([]);
+  const playerCollectionRef = collection(firestore, "Event");
+
+  useEffect(() => {
+    const getPlayers = async () => {
+      const data = await getDocs(playerCollectionRef);
+      updatePlayers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      console.log(data);
     };
-    this.handleShuffle = this.handleShuffle.bind(this);
-  }
 
-  handleShuffle(e) {
-    this.setState(randomizer(this.state.list));
-  }
+    getPlayers();
+  }, []);
 
+  function randomizer(players, matches) {
+    const names = []
+    for (let i=0; i < players.length; i++) {
+      names.push(players[i].name)
+    }
+    console.log(names);
+    var range = names.length;
+    while (range != 0) {
+      var random = Math.floor(Math.random() * range);
+      range--;
+      [names[range], names[random]] = [names[random], names[range]];
+    }
+    updateMatches(names)
+  };
 
-  render() {
-    return (
-      <div>
-        <h1> All Players </h1>
-        <PlayerList players={this.state.list}/>
-        <button onClick={this.handleShuffle}>Shuffle!</button>
-        <h1> Matches </h1>
-        <Match match={this.state.list}/>
+  return (
+    <div>
+
+      <h1>Bracket Maker</h1>
+      <Routes>
+        <Route path="/" element={<Player />} />,
+        <Route path="/view" element={<View />} />
+      </Routes>
+      <PlayerList players={players}/>
+      <div className="container mt-5">
+        <Button onClick={() => randomizer(players, matches) }>Generate Matches!</Button>
+        <p></p>
+        <Match match={matches}/>
+        <p></p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default App
+export default App;
